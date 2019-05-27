d8d58840612cc3be650330d4bc5f27a61762fffd
chiamakaobitube <obitubechiamaka@gmail.com>
2019-05-27 23:58:21 +0100

    feature(deleteCarEndpoint): admin delete car endpoint  - ensure that the delete car by id endpoint works correctly -ensure that there is a status code if deleted [Delivers #165983575]

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post('/car', carcontroller.createNewAd);
 router.get('/car', carcontroller.getAllCars);
 router.get('/car/:id', carcontroller.getSpecificCar);
-
+router.delete('/car/:id', carcontroller.deleteCar);
 
 export default router;
