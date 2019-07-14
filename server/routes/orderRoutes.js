7b49f85ec5a08b42d11ab5d85f6b83c1fc7b4d6a
chiamakaobitube <obitubechiamaka@gmail.com>
2019-07-14 23:55:36 +0100

    --amend

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -6,7 +6,7 @@ import Authentication from '../middleware/authToken';
 
 const router = Router();
 
-router.post('/order', Authentication, postOrderValidator, orderController.postOrder);
+router.post('/order', Authentication, orderController.postOrder);
 router.get('/order', Authentication, orderController.getAllOrders);
 router.get('/order/:id', Authentication, orderController.getSpecificOrder);
 router.patch('/order/:id/price', Authentication, orderController.updatePurchaseOrderPrice);
